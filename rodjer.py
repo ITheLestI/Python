9c6aadd5e13ab010bd2a6b0bf8b705232938dacc
lyubomyr83 <lyubomyr83@mail.ru>
2019-10-19 21:09:22 +0300

    rojer in progress...

--- rodjer.py
+++ rodjer.py
@@ -1,3 +1,7 @@
+from random import randint, choice
+from timeit import default_timer
+
+
 print("Привет, меня зовут Роджер, а как тебя?")
 name = input()
 name = name.title()
@@ -9,12 +13,17 @@ ready = ready.lower()
 
 while ready not in {'да', "нет"}:
     print("Введи да или нет")
-    play_again = input()
+    ready = input()
 
 if ready == "да":
     
     questions_quantity = ""  # количество примеров
     max_answer = ""  #  до скольки будем считать
+    mistakes = 0
+    correct_answers = 0
+    answers_time = 0
+
+
 
     while not questions_quantity.isdigit():
         print("Сколько примеров ты готов решить?")
@@ -43,6 +52,65 @@ if ready == "да":
                    max_answer = input()
         else:    
             print("Должна быть цифра")
+    
+    for i in range(int(questions_quantity)):
+        print(f"Пример {i+1}")
+        first = randint(1, int(max_answer))
+        second = randint(1, int(max_answer))
+        sign = choice("+-")
+        
+        if sign == '-':
+            while first<second:
+                first = randint(1, int(max_answer))
+                second = randint(1, int(max_answer))
+            correct = first-second
+        
+        if sign == "+":
+            while first+second>int(max_answer):
+                first = randint(1, int(max_answer))
+                second = randint(1, int(max_answer))
+            correct = first+second
+
+        
+        print(first, sign, second)
+        start =  default_timer()
+        answer = input()
+        stop = default_timer
+        answers_time+=stop-start
+
+        while not answer.isdigit():
+            print("Должна быть цифра")
+            start =  default_timer()
+            answer = input()
+            stop = default_timer
+            answers_time+=stop-start
+            
+        
+        if int(answer) == correct:
+            print("Правильно, молодец!")
+            print("")
+            correct_answers+=1
+        else:
+            print("Неправильно")
+            print(f"Правильный ответ: {correct}")
+            print("")
+            mistakes+=1
+    
+
+    if answers_time<60:
+        time_spent = answers_time
+    else:
+        minutes = str(round(answers_time/60))
+        seconds =str(answers_time-(minutes*60))
+    
+
+
+
+    if mistakes == 0:
+        print("Молодец, " +name+ ". Ты правильно ответил на все вопросы.")
+    else:
+        print("Правильных ответов:"+str(correct_answers))
+        print("Неправильных ответов:"+str(mistakes))
 else:
     print("""Передумал? Хорошо, может как-нибудь в следующий раз...
 Пока!""")
