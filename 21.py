3ec2f095925c14012775c4a7a9e4189f7072b48b
lyubomyr83 <lyubomyr83@mail.ru>
2020-03-14 21:03:57 +0300

    140320

--- 21.py
+++ 21.py
@@ -1,45 +1,106 @@
 from random import shuffle
-koloda = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]*4
-shuffle(koloda)
+from time import sleep
 
-your_points = 0
-dealer_points = 0
-min_dealer_points = 17
-move = 1
+play_again = "да"
 
-more = 'да'
+while play_again == "да":
 
-while your_points < 21 and dealer_points < 17:
-    dealer_card = koloda.pop()
-    dealer_points += dealer_card
-    print(1111)
-    if move == 1:
-        cards = []
-        for i in range(2):
+    koloda = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]*4
+    shuffle(koloda)
+    your_points = 0
+    dealer_points = 0
+    min_dealer_points = 17
+    move = 1
+    choice = 'да'
+
+    while choice == "да":
+        
+        if move == 1:
+            dealer_card = koloda.pop()
+            dealer_points += dealer_card
+
+            cards = []
+            for i in range(2):
+                your_card = koloda.pop()
+                your_points += your_card
+                cards.append(your_card)
+        else:
             your_card = koloda.pop()
             your_points += your_card
-            cards.append(your_card)
+
+        print(f"""
+        =============
+        ОЧКИ:
+        Крупье: {dealer_points}
+        Вы: {your_points}
+        =============    
+            """)
+        if move == 1:
+            print(f"    Вам выпали карты номиналом {cards[0]} и {cards[1]}")
+        else:
+            print(f"Вам выпала карта номиналом {your_card}")
+
+        if your_points > 21:
+            print(f"""
+        =============
+        ОЧКИ:
+        Крупье: {dealer_points}
+        Вы: {your_points}
+        =============    
+            """)
+            print("Вы проиграли")
+            break
+        elif your_points == 21:
+            choice = "нет"
+        else:
+            choice = input("Ещё? (да/нет)\n")
+            while choice not in ["да", "нет"]:
+                choice  = input("Введи да или нет\n")
+
+        move+=1
+        
     else:
-        your_card = koloda.pop()
-        your_points += your_card
+        while dealer_points <= 17:
+            dealer_card = koloda.pop()
+            dealer_points += dealer_card
+            print(f"Крупье вытянул карту номиналом {dealer_card}")
+            sleep(2)
 
-    print(f"""
-    =============
-    ОЧКИ:
-    Крупье: {dealer_points}
-    Вы: {your_points}
-    =============    
-        """)
-    print(f"    Вам выпали карты номиналом {cards[0]} и {cards[1]}")
-    print("Ещё? (да/нет)\n")
-    
-    choice = input()
-    while choice not in ["да", "нет"]:
-        print("Введи да или нет")
-        choce  = input()
-    if choice == ""
-
-
-    move+=1
-    
-    
+        if your_points == dealer_points:
+            print(f"""
+        =============
+        ОЧКИ:
+        Крупье: {dealer_points}
+        Вы: {your_points}
+        =============    
+            """)
+            print("Ничья")
+        elif dealer_points > 21:
+            print(f"""
+        =============
+        ОЧКИ:
+        Крупье: {dealer_points}
+        Вы: {your_points}
+        =============    
+            """)
+            print("Вы выиграли")
+        else:
+            if your_points > dealer_points:
+                print(f"""
+        =============
+        ОЧКИ:
+        Крупье: {dealer_points}
+        Вы: {your_points}
+        =============    
+            """)
+                print("Вы выиграли")
+            else:
+                print(f"""
+        =============
+        ОЧКИ:
+        Крупье: {dealer_points}
+        Вы: {your_points}
+        =============    
+            """)
+                print("Вы проиграли")
+    play_again = input("Сыграем еще? Да/нет\n")
